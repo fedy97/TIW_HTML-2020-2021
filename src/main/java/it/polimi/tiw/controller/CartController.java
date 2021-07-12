2ef8eb0f4bbc56fb43f96c2727d6828ba5684f32
fedy97 <frc.morreale@gmail.com>
2021-07-12 16:16:58 +0200

    fixed error when opening /cart if cart is empty

--- src/main/java/it/polimi/tiw/controller/CartController.java
+++ src/main/java/it/polimi/tiw/controller/CartController.java
@@ -1,6 +1,21 @@
 
 package it.polimi.tiw.controller;
 
+import it.polimi.tiw.bean.*;
+import it.polimi.tiw.dao.ArticleDAO;
+import it.polimi.tiw.dao.SellerDAO;
+import it.polimi.tiw.dao.ShipmentPolicyDAO;
+import it.polimi.tiw.utils.GenericServlet;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -9,29 +24,12 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import javax.servlet.ServletContext;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.thymeleaf.context.WebContext;
-
-import it.polimi.tiw.bean.*;
-import it.polimi.tiw.dao.ArticleDAO;
-import it.polimi.tiw.dao.SellerDAO;
-import it.polimi.tiw.dao.ShipmentPolicyDAO;
-import it.polimi.tiw.utils.GenericServlet;
-
 @WebServlet("/cart")
 public class CartController extends GenericServlet {
 
-    private static final Logger log              = LoggerFactory.getLogger(CartController.class.getSimpleName());
+    private static final Logger log = LoggerFactory.getLogger(CartController.class.getSimpleName());
 
-    private static final long   serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     public CartController() {
 
@@ -69,8 +67,9 @@ public class CartController extends GenericServlet {
     }
 
     private Map<String, OrderBean> buildCartModel(Map<String, List<ArticleBean>> articles) {
-
         Map<String, OrderBean> orderBeanMap = new HashMap<>();
+        if (articles == null)
+            return orderBeanMap;
         log.debug("Articles --> {}", articles);
         log.debug("Articles size --> {}", articles.size());
         articles.forEach((seller, articleList) -> {
